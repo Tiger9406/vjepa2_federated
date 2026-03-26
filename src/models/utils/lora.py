4e33262c53caeb5968171314cf38cf6d7bcf0708
Tiger <xcao2005@gmail.com>
2026-03-26 18:05:45 +0000

    define lora module as an nn.Linear appended with global and local adapters

---
+++ src/models/utils/lora.py
@@ -0,0 +1,54 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+class LoRALinear(nn.Module):
+    """
+    Wraps an existing nn.Linear with two parallel LoRA adapters:
+    - global lora
+    - local lora
+    """
+
+    def __init__(self, linear: nn.Linear, r: int = 8, alpha: float = 16.0):
+        """
+        linear: frozen weights
+        r: rank; size of bottleneck between A & B Lora matrices
+        alpha: scaling parameter; how much influence lora has on linear
+            - alpha = 16, rank = 8, scale is 2; new knowledge doubled in strength
+        """
+        super().__init__()
+        self.linear = linear  # frozen base weight
+
+        d_in, d_out = linear.in_features, linear.out_features
+        scale = alpha / r
+
+        # global adapter
+        self.global_A = nn.Parameter(torch.empty(r, d_in))  # compresses input data
+        self.global_B = nn.Parameter(
+            torch.zeros(d_out, r)
+        )  # expands out into output size
+
+        # local adapter
+        self.local_A = nn.Parameter(torch.empty(r, d_in))
+        self.local_B = nn.Parameter(torch.zeros(d_out, r))
+
+        # function for calculating best random starting values
+        nn.init.kaiming_uniform_(self.global_A, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.local_A, a=math.sqrt(5))
+
+        self.scale = scale
+
+    def forward(self, x):
+        """
+        Operations performed on input x
+        """
+        base = self.linear(x)  # runs through base layer
+
+        # runs through the Loras
+        global_res = (x @ self.global_A.T) @ self.global_B.T
+        local_res = (x @ self.local_A.T) @ self.local_B.T
+
+        # simply adds on
+        return base + self.scale * (global_res + local_res)
