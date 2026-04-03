16c97d10998ba9372db1e64ddfad89dc2f1e4bc8
Tiger <xcao2005@gmail.com>
2026-04-03 03:19:49 +0000

    move to ram

--- eval_ucf101/get_data.bash
+++ eval_ucf101/get_data.bash
@@ -6,4 +6,6 @@ wget --no-check-certificate -P eval_ucf101/ https://www.crcv.ucf.edu/data/UCF101
 unrar x eval_ucf101/UCF101.rar eval_ucf101/
 unzip eval_ucf101/UCF101TrainTestSplits-RecognitionTask.zip -d eval_ucf101/
 
-echo "Data successfully prepared in eval_ucf101/"
+rm eval_ucf101/UCF101.rar eval_ucf101/UCF101TrainTestSplits-RecognitionTask.zip
+
+echo "Data successfully prepared in eval_ucf101/; deleted zip"
