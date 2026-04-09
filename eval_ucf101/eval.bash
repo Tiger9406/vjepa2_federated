d0cd5a79c6ccb7acf4c522ac9a2ace5b16a05306
Tiger <xcao2005@gmail.com>
2026-04-09 11:36:10 -0400

    remove unused scripts

--- eval_ucf101/eval.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m evals.main --fname eval_ucf101/eval_ucf101.yaml --checkpoint /workspace/vjepa_run/latest.pt 
---
+++ fed_training/restore_pretrained.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+REPO_ID="tiger9406/fed-client-dataset"
+LOCAL_DIR="/content/vjepa2_federated/fed_training"
+
+python3 -c "
+import os
+from huggingface_hub import hf_hub_download
+hf_hub_download(
+    repo_id='$REPO_ID',
+    filename='latest.pt',
+    repo_type='dataset',
+    local_dir='$LOCAL_DIR',
+    token=os.environ.get('HF_TOKEN')
+)
+"
+echo "Done. Downloaded to $LOCAL_DIR/latest.pt"
